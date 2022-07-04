fb6355489a5596d9941c341dcd738c28d67dcf0f
lancebisenio <lancebisenio@gmail.com>
2022-07-04 23:06:41 +0800

    Initial file

--- sanity_ecommerce/schemas/schema.js
+++ sanity_ecommerce/schemas/schema.js
@@ -7,7 +7,5 @@ import banner from './banner'
 export default createSchema({
   // We name our schema
   name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([ product, banner ]),
 })
